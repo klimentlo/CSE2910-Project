314c3cde5e7c95724e1ddcf763b3e3d93b6947c1
k.lo1 <k.lo1@share.epsb.ca>
2023-11-30 11:48:47 -0700

    basic things

--- my_unit.py
+++ my_unit.py
@@ -1,10 +1,12 @@
 # myunits.py
 from my_sprite import MySprite
-
+from image_sprite import ImageSprite
 class MyUnits(MySprite):
-    def __init__(self, WIDTH, HEIGHT, MAX_HEALTH, ATTACK, LEVEL=1):
+    def __init__(self,FILENAME, WIDTH, HEIGHT, MAX_HEALTH, ATTACK, LEVEL=1):
         MySprite.__init__(self, WIDTH, HEIGHT)
+        self.__UNIT = ImageSprite(FILENAME)
         self.__MAX_HEALTH = MAX_HEALTH
         self.__CURENT_HEALTH = MAX_HEALTH
         self.__LEVEL = LEVEL
         self.__ATTACK = ATTACK
+
