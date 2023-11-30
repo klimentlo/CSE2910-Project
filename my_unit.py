3b265c5fe5f49d408eee3b0233a4e4f407f242e9
k.lo1 <k.lo1@share.epsb.ca>
2023-11-30 11:43:30 -0700

    basic things

--- my_unit.py
+++ my_unit.py
@@ -2,7 +2,9 @@
 from my_sprite import MySprite
 
 class MyUnits(MySprite):
-    def __init__(self, WIDTH, HEIGHT, MAX_HEALTH):
+    def __init__(self, WIDTH, HEIGHT, MAX_HEALTH, ATTACK, LEVEL=1):
         MySprite.__init__(self, WIDTH, HEIGHT)
         self.__MAX_HEALTH = MAX_HEALTH
         self.__CURENT_HEALTH = MAX_HEALTH
+        self.__LEVEL = LEVEL
+        self.__ATTACK = ATTACK
