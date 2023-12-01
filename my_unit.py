f46275843fc6276d34b3d83fffcc63f3a5cce49f
k.lo1 <k.lo1@share.epsb.ca>
2023-12-01 12:37:40 -0700

    basic things

--- my_unit.py
+++ my_unit.py
@@ -2,11 +2,35 @@
 from my_sprite import MySprite
 from image_sprite import ImageSprite
 class MyUnits(MySprite):
-    def __init__(self,FILENAME, WIDTH, HEIGHT, MAX_HEALTH, ATTACK, LEVEL=1):
-        MySprite.__init__(self, WIDTH, HEIGHT)
+    def __init__(self, FILENAME, WIDTH, HEIGHT, X, SPEED, SPAWN_COOLDOWN, MAX_HEALTH, RANGE, ATTACK, ATTACK_COOLDOWN, UNIT_TYPE, LEVEL=1):
+                                            #   Y \/
+        MySprite.__init__(self, WIDTH, HEIGHT, X, 300, SPEED)
         self.__UNIT = ImageSprite(FILENAME)
+        # will have to add the skill point modifiers later
+        self.__SPAWN_COOLDOWN = SPAWN_COOLDOWN
         self.__MAX_HEALTH = MAX_HEALTH
         self.__CURENT_HEALTH = MAX_HEALTH
+        self.__RANGE = RANGE
+        self.__ATTACKS = []
+        self.__ATTACK_COOLDOWN = ATTACK_COOLDOWN
+        self.__ALIVE = True
+        self.__UNIT_TYPE = UNIT_TYPE
         self.__LEVEL = LEVEL
-        self.__ATTACK = ATTACK
 
+    def takeDamage(self, DAMAGE):
+        self.__CURENT_HEALTH -= DAMAGE
+
+    def increaseLevel(self):
+        self.__LEVEL += 1
+
+#    def canAtack(self):
+#    enemyUnits = [Enemy(), Enemy(), Enemy(), Enemy(), Enemy()]
+#        for ENEMIES in enemyUnits:
+
+    def addAttack(self, ATTACK):
+        self.__ATTACKS.append(ATTACK)
+
+    def isAlive(self):
+        if self.__ALIVE == False:
+            # will eventually pop it off, which will disappear
+            print("Dead")
