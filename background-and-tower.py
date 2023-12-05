81693ed9f24d55c0902aaa8f5bdc6fa8f112de8e
s.tran18 <s.tran18>
2023-12-05 11:42:01 -0700

    added background and human base

--- background-and-tower.py
+++ background-and-tower.py
@@ -5,19 +5,19 @@ date: 2023-12-4
 """
 import pygame
 from image_sprite import ImageSprite
-from my_sprite import MySprite
-
-class Background:
-    def __init__(self):
-        self._SEA = ImageSprite("media/soft-underwater-landscape-with-waves-seabed-vector-22615902.jpg")
 
 
 if __name__ == "__main__":
     from window import Window
     pygame.init()
-    WINDOW = Window("background", 1000, 500, 30)
-    BACKGROUND = ImageSprite("media/drawing-underwater-world-poster-background_2752998.jpg")
+    WINDOW = Window("background", 960, 400, 30)
+    BACKGROUND = ImageSprite("media/b228ea58-42fe-43eb-9f60-07622a4072f9.png")
     BACKGROUND.setScale(1)
+    BACKGROUND.setY(-500)
+    E_TOWER = ImageSprite("media/image-removebg-preview.png")
+    E_TOWER.setScale(2/5)
+    E_TOWER.setY(200)
+
 
     while True:
         for event in pygame.event.get():
@@ -28,4 +28,5 @@ if __name__ == "__main__":
 
         WINDOW.clearScreen()
         WINDOW.getSurface().blit(BACKGROUND.getSurface(), BACKGROUND.getPOS())
+        WINDOW.getSurface().blit(E_TOWER.getSurface(), E_TOWER.getPOS())
         WINDOW.updateFrame()
