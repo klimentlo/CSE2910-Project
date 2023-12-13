b52d1ef58c27d1dcd2479ef68dec6da59f1544e0
k.lo1 <k.lo1@share.epsb.ca>
2023-12-13 11:49:51 -0700

    weird error idk

--- unit_attacks.py
+++ unit_attacks.py
@@ -3,18 +3,16 @@ TITLE: UNIT ATTACK CLASS
 AUTHOR : samuel tran
 date: 2023-12-1
 """
+from my_sprite import MySprite
+from image_sprite import ImageSprite
 
-class Attacks:
-    def __init__(self, NAME, DAMAGE, RANGE, COOLDOWN):
-        """
-
-        :param NAME: str
-        :param DAMAGE: int
-        """
-        self._NAME = NAME
+class Attacks(MySprite):
+    def __init__(self, FILENAME, DAMAGE, RANGE):
+        MySprite.__init__(self)
+        self._SPRITE = ImageSprite(FILENAME)
         self._DAMAGE = DAMAGE
         self._RANGE = RANGE
-        self._COOLDOWN = COOLDOWN
+        self._SURFACE = self._SPRITE.getSurface()
 
     # -- MODIFERS METHODS --#
     # -- ACCESSOR METHODS  -- #
@@ -27,26 +25,24 @@ class Attacks:
     def getRange(self):
         return self._RANGE
 
-    def getCooldown(self):
-        return self._COOLDOWN
-
 moveSet = {
     # fish attacks
-    "flop": Attacks("Flop", 60, 45, 10),
-    "sting": Attacks("Sting", 60, 45, 10),
-    "swordSlash": Attacks("Sword Slash", 40, 45, 2),
-    "starShine": Attacks("Star Shine", 60, 45, 10),
-    "bubbleBeam": Attacks("Bubble Beam", 60, 45, 10),
-    "chomp": Attacks("Chomp", 60, 45, 10),
-    "acidShot": Attacks("Acid Shot", 40, 45, 123),
-    "devour": Attacks("Devour", 60, 45, 10),
+    "flop": Attacks("media/scuba1.png", 60, 45),
+    "sting": Attacks(60, 45),
+    "swordSlash": Attacks(40, 45),
+    "starShine": Attacks(60, 45),
+    "bubbleBeam": Attacks(60, 45),
+
+    #"chomp": Attacks("Chomp", 60, 45, 10),
+    #"acidShot": Attacks("Acid Shot", 40, 45, 123),
+    #"devour": Attacks("Devour", 60, 45, 10),
     # human attacks
     "picture": Attacks("Picture", 60, 45, 10),
     "water push": Attacks("Water Push", 60, 45, 10), # use there strong legs to kick the fish back
     "spear throw": Attacks("Spear Throw", 60, 45, 10),
     "net throw": Attacks("Net Throw", 60, 45, 10),
     "vile fluid": Attacks("Vile Fluid", 35, 45,23),
-    "ram": Attacks("Ram", 35, 45,23),
-    "missile": Attacks("Missile", 35, 45,23),
-    "eat": Attacks("Eat", 35, 45,23),
+    #"ram": Attacks("Ram", 35, 45,23),
+    #"missile": Attacks("Missile", 35, 45,23),
+    #"eat": Attacks("Eat", 35, 45,23),
 }
