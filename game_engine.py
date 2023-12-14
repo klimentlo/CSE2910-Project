87a469585f233df956f2ddce9b6c20e0a4f74af0
k.lo1 <k.lo1@share.epsb.ca>
2023-12-14 11:50:44 -0700

    finsish human sprites

--- game_engine.py
+++ game_engine.py
@@ -5,11 +5,12 @@ from my_sprite import MySprite
 from my_unit import MyUnit
 from text import Text
 from unit_attacks import Attacks
-from unit_attacks import moveSet
 from window import Window
 import time, random, copy
 import pygame
 pygame.init()
+
+
 
 class Game():
     '''
@@ -30,10 +31,15 @@ class Game():
         self.__PREVIOUS_TIME = self.__TIME
         self.__TIME_PASSED = 0.1
 
+        # - - - - - - - - - - - - - - - - - - - - #
+        #   -  -  - FISH CONFIGURATIONS -  -  -   #
+        # - - - - - - - - - - - - - - - - - - - - #
+
         # Fish Spawning Cooldown
         self.__FISH_SPAWN_COOLDOWN = [2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]
         self.__FISH_CURRENT_SPAWN_COOLDOWN = [2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]
-        # (self, FILENAME, X, SPEED, MAX_HEALTH, RANGE, ATTACK, ATTACK_COOLDOWN, UNIT_TYPE, LEVEL=1)
+
+
 
         self.__X_SPAWN_LOCATION = 900
         self.__FISH_SPEED = [1, 2, 3, 4, 5]
@@ -41,23 +47,61 @@ class Game():
         self.__FISH_RANGE = [50, 100, 150, 200, 250]
         self.__FISH_ATTACK_COOLDOWN = [1, 2, 3, 4, 5]
 
-        self.__SALMON = MyUnit("media/image-removebg-preview.png", self.__X_SPAWN_LOCATION, self.__FISH_SPEED[0], self.__FISH_MAX_HEALTH[0], self.__FISH_RANGE[0], moveSet["flop"], self.__FISH_ATTACK_COOLDOWN[0], "Fish", -1)
+
+                                                # (self, FILENAME, X, SPEED, MAX_HEALTH, RANGE, ATTACK, ATTACK_COOLDOWN, UNIT_TYPE, LEVEL=1)
+        self.__SALMON = MyUnit("media/image-removebg-preview.png", self.__X_SPAWN_LOCATION, self.__FISH_SPEED[0], self.__FISH_MAX_HEALTH[0], self.__FISH_RANGE[0], Attacks("media/humanBase.png", 60, 45, 5, -1), self.__FISH_ATTACK_COOLDOWN[0], "Fish", -1)
         self.__SALMON_COST = 50
         self.__SALMON.setScale(0.5)
 
-        self.__STING_RAY = MyUnit("media/scuba1.png", self.__X_SPAWN_LOCATION, self.__FISH_SPEED[1], self.__FISH_MAX_HEALTH[1], self.__FISH_RANGE[1], moveSet["sting"], self.__FISH_ATTACK_COOLDOWN[1], "Fish", -1)
+        self.__STING_RAY = MyUnit("media/scuba1.png", self.__X_SPAWN_LOCATION, self.__FISH_SPEED[1], self.__FISH_MAX_HEALTH[1], self.__FISH_RANGE[1], Attacks("media/humanBase.png", 60, 45, 5, -1), self.__FISH_ATTACK_COOLDOWN[1], "Fish", -1)
         self.__STING_RAY_COST = 100
         self.__STING_RAY.setScale(0.5)
 
-        self.__SWORD_FISH = MyUnit("media/submarine2.png", self.__X_SPAWN_LOCATION, self.__FISH_SPEED[2], self.__FISH_MAX_HEALTH[2], self.__FISH_RANGE[2], moveSet["sting"], self.__FISH_ATTACK_COOLDOWN[2], "Fish", -1)
+        self.__SWORD_FISH = MyUnit("media/submarine2.png", self.__X_SPAWN_LOCATION, self.__FISH_SPEED[2], self.__FISH_MAX_HEALTH[2], self.__FISH_RANGE[2], Attacks("media/humanBase.png", 60, 45, 5, -1), self.__FISH_ATTACK_COOLDOWN[2], "Fish", -1)
         self.__SWORD_FISH_COST = 150
         self.__SWORD_FISH.setScale(0.5)
 
-        self.__STAR_FISH = MyUnit("media/catpaws.jpg", self.__X_SPAWN_LOCATION, self.__FISH_SPEED[3], self.__FISH_MAX_HEALTH[3], self.__FISH_RANGE[3], moveSet["sting"], self.__FISH_ATTACK_COOLDOWN[3], "Fish", -1)
+        self.__STAR_FISH = MyUnit("media/catpaws.jpg", self.__X_SPAWN_LOCATION, self.__FISH_SPEED[3], self.__FISH_MAX_HEALTH[3], self.__FISH_RANGE[3], Attacks("media/humanBase.png", 60, 45, 5, -1), self.__FISH_ATTACK_COOLDOWN[3], "Fish", -1)
         self.__STAR_FISH_COST = 250
         self.__STAR_FISH.setScale(0.1)
 
-        self.__SEA_HORSE = MyUnit("media/humanBase.png", self.__X_SPAWN_LOCATION, self.__FISH_SPEED[4], self.__FISH_MAX_HEALTH[4], self.__FISH_RANGE[4],moveSet["sting"], self.__FISH_ATTACK_COOLDOWN[4], "Fish", -1)
+        self.__SEA_HORSE = MyUnit("media/humanBase.png", self.__X_SPAWN_LOCATION, self.__FISH_SPEED[4], self.__FISH_MAX_HEALTH[4], self.__FISH_RANGE[4], Attacks("media/humanBase.png", 60, 45, 5, -1), self.__FISH_ATTACK_COOLDOWN[4], "Fish", -1)
+        self.__SEA_HORSE_COST = 400
+        self.__SEA_HORSE.setScale(0.25)
+
+
+        # - - - - - - - - - - - - - - - - - - - - - #
+        #    -  -  - HUMAN CONFIGURATIONS -  -  -   #
+        # - - - - - - - - - - - - - - - - - - - - - #
+
+        # Human Spawning Cooldown
+        self.__HUMAN_SPAWN_COOLDOWN = [2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]
+        self.__HUMAN_CURRENT_SPAWN_COOLDOWN = [2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0]
+
+        self.__X_SPAWN_LOCATION = 900
+        self.__HUMAN_SPEED = [1, 2, 3, 4, 5]
+        self.__HUMAN_MAX_HEALTH = [50, 100, 150, 200, 250]
+        self.__HUMAN_RANGE = [50, 100, 150, 200, 250]
+        self.__HUMAN_ATTACK_COOLDOWN = [1, 2, 3, 4, 5]
+
+        # (self, FILENAME, X, SPEED, MAX_HEALTH, RANGE, ATTACK, ATTACK_COOLDOWN, UNIT_TYPE, LEVEL=1)
+        self.__SALMON = MyUnit("media/image-removebg-preview.png", self.__X_SPAWN_LOCATION, self.__HUMAN_SPEED[0],self.__HUMAN_MAX_HEALTH[0], self.__HUMAN_RANGE[0],Attacks("media/humanBase.png", 60, 45, 5, -1), self.__HUMAN_ATTACK_COOLDOWN[0], -1)
+        self.__SALMON_COST = 50
+        self.__SALMON.setScale(0.5)
+
+        self.__STING_RAY = MyUnit("media/scuba1.png", self.__X_SPAWN_LOCATION, self.__HUMAN_SPEED[1],self.__HUMAN_MAX_HEALTH[1], self.__HUMAN_RANGE[1],Attacks("media/humanBase.png", 60, 45, 5, -1), self.__HUMAN_ATTACK_COOLDOWN[1], -1)
+        self.__STING_RAY_COST = 100
+        self.__STING_RAY.setScale(0.5)
+
+        self.__SWORD_HUMAN = MyUnit("media/submarine2.png", self.__X_SPAWN_LOCATION, self.__HUMAN_SPEED[2],self.__HUMAN_MAX_HEALTH[2], self.__HUMAN_RANGE[2],Attacks("media/humanBase.png", 60, 45, 5, -1), self.__HUMAN_ATTACK_COOLDOWN[2], -1)
+        self.__SWORD_HUMAN_COST = 150
+        self.__SWORD_HUMAN.setScale(0.5)
+
+        self.__STAR_HUMAN = MyUnit("media/catpaws.jpg", self.__X_SPAWN_LOCATION, self.__HUMAN_SPEED[3],self.__HUMAN_MAX_HEALTH[3], self.__HUMAN_RANGE[3],Attacks("media/humanBase.png", 60, 45, 5, -1), self.__HUMAN_ATTACK_COOLDOWN[3], -1)
+        self.__STAR_HUMAN_COST = 250
+        self.__STAR_HUMAN.setScale(0.1)
+
+        self.__SEA_HORSE = MyUnit("media/humanBase.png", self.__X_SPAWN_LOCATION, self.__HUMAN_SPEED[4],self.__HUMAN_MAX_HEALTH[4], self.__HUMAN_RANGE[4],Attacks("media/humanBase.png", 60, 45, 5, -1), self.__HUMAN_ATTACK_COOLDOWN[4], -1)
         self.__SEA_HORSE_COST = 400
         self.__SEA_HORSE.setScale(0.25)
 
@@ -109,6 +153,8 @@ class Game():
                 KEYSPRESSED = pygame.key.get_pressed()
                 HUMAN.WASDmove(KEYSPRESSED)
 
+            for ATTACK in self.__LIVE_ATTACKS:
+                ATTACK.marqueeX(self.__WINDOW.getWidth())
 
 
             self.__fishAttack()
@@ -196,25 +242,23 @@ class Game():
 
         if KEYPRESSED[pygame.K_q] == 1:
             # Salmon
-            UNIT = MyUnit("media/image-removebg-preview.png", SPAWN, 5, 500, 100, moveSet["flop"], 2, "Human")
+            UNIT = MyUnit("media/image-removebg-preview.png", SPAWN, 5, 500, 100, Attacks("media/humanBase.png", 60, 45), 2, "Human")
         if KEYPRESSED[pygame.K_w] == 1:
             # Stingray
-            UNIT = MyUnit("media/pngtree-an-orange-cat-with-squinting-eyes-png-image_2664925.jpg", SPAWN, 5, 500, 100,
-                          moveSet["sting"], 2, "Human")
+            UNIT = MyUnit("media/pngtree-an-orange-cat-with-squinting-eyes-png-image_2664925.jpg", SPAWN, 5, 500, 100, Attacks("media/humanBase.png", 60, 45), 2, "Human")
 
         if KEYPRESSED[pygame.K_e] == 1:
-            UNIT = MyUnit("media/pngtree-an-orange-cat-with-squinting-eyes-png-image_2664925.jpg", SPAWN, 5, 500, 100,
-                          moveSet["flop"], 2, "Human")
+            UNIT = MyUnit("media/pngtree-an-orange-cat-with-squinting-eyes-png-image_2664925.jpg", SPAWN, 5, 500, 100, Attacks("media/humanBase.png", 60, 45), 2, "Human")
         if KEYPRESSED[pygame.K_r] == 1:
-            UNIT = MyUnit("media/scuba1.png", SPAWN, 5, 500, 100, moveSet["flop"], 2, "Human")
+            UNIT = MyUnit("media/scuba1.png", SPAWN, 5, 500, 100, Attacks("media/humanBase.png", 60, 45), 2, "Human")
         if KEYPRESSED[pygame.K_t] == 1:
-            UNIT = MyUnit("media/scuba2.png", SPAWN, 5, 500, 100, moveSet["flop"], 2, "Human")
+            UNIT = MyUnit("media/scuba2.png", SPAWN, 5, 500, 100, Attacks("media/humanBase.png", 60, 45), 2, "Human")
         if KEYPRESSED[pygame.K_y] == 1:
-            UNIT = MyUnit("media/scuba 3.png", SPAWN, 5, 500, 100, moveSet["flop"], 2, "Human")
+            UNIT = MyUnit("media/scuba 3.png", SPAWN, 5, 500, 100, Attacks("media/humanBase.png", 60, 45), 2, "Human")
         if KEYPRESSED[pygame.K_u] == 1:
-            UNIT = MyUnit("media/humanBase.png", SPAWN, 5, 500, 100, moveSet["flop"], 2, "Human")
+            UNIT = MyUnit("media/humanBase.png", SPAWN, 5, 500, 100, Attacks("media/humanBase.png", 60, 45), 2, "Human")
         if KEYPRESSED[pygame.K_i] == 1:
-            UNIT = MyUnit("media/image-removebg-preview.png", SPAWN, 5, 500, 100, moveSet["flop"], 2, "Human")
+            UNIT = MyUnit("media/image-removebg-preview.png", SPAWN, 5, 500, 100, Attacks("media/humanBase.png", 60, 45), 2, "Human")
         try:
             UNIT.setY(300 - UNIT.getHeight())
             return UNIT
@@ -239,14 +283,30 @@ class Game():
 
         self.__WINDOW.updateFrame()
 
+
     def __fishAttack(self):
         for FISH in self.__DEPlOYED_FISHES:
             if FISH.getSpeed() == 0: # if their movement speed is zero
                 FISH.getAttackCooldown()
                 if FISH.getCurrentAttackCooldown() >= FISH.getAttackCooldown():
-                    self.__LIVE_ATTACKS.append(moveSet["flop"])
+                    ATTACK = copy.copy(FISH.getAttack())
+                    self.__LIVE_ATTACKS.append(ATTACK)
+                    self.__LIVE_ATTACKS[-1].setX(FISH.getX()-ATTACK.getWidth())
+                    self.__LIVE_ATTACKS[-1].setY(FISH.getY()+FISH.getWidth()//2-ATTACK.getWidth()//2)
+                    print(self.__LIVE_ATTACKS)
                     FISH.resetAttackCooldown()
 
+    def __HumanAttack(self):
+        for HUMAN in self.__DEPLOYED_HUMANS:
+            if HUMAN.getSpeed() == 0: # if their movement speed is zero
+                HUMAN.getAttackCooldown()
+                if HUMAN.getCurrentAttackCooldown() >= HUMAN.getAttackCooldown():
+                    ATTACK = copy.copy(HUMAN.getAttack())
+                    self.__LIVE_ATTACKS.append(ATTACK)
+                    self.__LIVE_ATTACKS[-1].setX(HUMAN.getX())
+                    self.__LIVE_ATTACKS[-1].setY(HUMAN.getY()+HUMAN.getWidth()//2-ATTACK.getWidth()//2)
+                    print(self.__LIVE_ATTACKS)
+                    HUMAN.resetAttackCooldown()
 
 
 
