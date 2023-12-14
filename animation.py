1f0f4d245696627145f9c25316af4af0d05c5088
s.tran18 <s.tran18>
2023-12-14 11:50:28 -0700

    added animation for scuba diver

--- animation.py
+++ animation.py
@@ -1,30 +1,75 @@
 import pygame
 
-class Player(pygame.sprite.Sprite):
+class Scuda(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
         super().__init__()
         self.sprites = []
-        self.sprites.append(pygame.image.load('media/00.png'))
-        self.sprites.append(pygame.image.load('media/01.png'))
-        self.sprites.append(pygame.image.load('media/02.png'))
-        self.sprites.append(pygame.image.load('media/03.png'))
-        self.sprites.append(pygame.image.load('media/10.png'))
-        self.sprites.append(pygame.image.load('media/11.png'))
-        self.sprites.append(pygame.image.load('media/12.png'))
-        self.sprites.append(pygame.image.load('media/13.png'))
-        self.sprites.append(pygame.image.load('media/20.png'))
-        self.sprites.append(pygame.image.load('media/21.png'))
-        self.sprites.append(pygame.image.load('media/22.png'))
-        self.sprites.append(pygame.image.load('media/23.png'))
-        self.sprites.append(pygame.image.load('media/30.png'))
-        self.sprites.append(pygame.image.load('media/31.png'))
-        self.sprites.append(pygame.image.load('media/32.png'))
-        self.sprites.append(pygame.image.load('media/33.png'))
+        self.is_animating = False
+        self.sprites.append(pygame.image.load('media/00-removebg-preview.png'))
+        self.sprites.append(pygame.image.load('media/01-removebg-preview.png'))
+        self.sprites.append(pygame.image.load('media/02-removebg-preview.png'))
+        self.sprites.append(pygame.image.load('media/03-removebg-preview.png'))
+        self.sprites.append(pygame.image.load('media/10-removebg-preview.png'))
+        self.sprites.append(pygame.image.load('media/11-removebg-preview.png'))
+        self.sprites.append(pygame.image.load('media/12-removebg-preview.png'))
+        self.sprites.append(pygame.image.load('media/13-removebg-preview.png'))
+        self.sprites.append(pygame.image.load('media/20-removebg-preview.png'))
+        self.sprites.append(pygame.image.load('media/21-removebg-preview.png'))
+        self.sprites.append(pygame.image.load('media/22-removebg-preview.png'))
+        self.sprites.append(pygame.image.load('media/23-removebg-preview.png'))
+        self.sprites.append(pygame.image.load('media/30-removebg-preview.png'))
+        self.sprites.append(pygame.image.load('media/31-removebg-preview.png'))
+        self.sprites.append(pygame.image.load('media/32-removebg-preview.png'))
+        self.sprites.append(pygame.image.load('media/33-removebg-preview.png'))
         self.current_sprite = 0
         self.image = self.sprites[self.current_sprite]
 
         self.rect = self.image.get_rect()
         self.rect.topleft = [pos_x, pos_y]
+
+
+    def animate(self):
+        self.is_animating = True
+
+
+    def update(self):
+        if self.is_animating == True:
+            self.current_sprite += 0.2
+
+            if self.current_sprite >= len(self.sprites):
+                self.current_sprite = 0
+                self.is_animating = False
+
+            self.image = self.sprites[int(self.current_sprite)]
+
+class Boat(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__()
+        self.sprites = []
+        self.is_animating = False
+        self.sprites.append(pygame.image.load('media/submarine2.png'))
+        self.sprites.append(pygame.image.load('media/submarine4.png'))
+        self.current_sprite = 0
+        self.image = self.sprites[self.current_sprite]
+
+        self.rect = self.image.get_rect()
+        self.rect.topleft = [pos_x, pos_y]
+
+
+    def animate(self):
+        self.is_animating = True
+
+
+    def update(self):
+        if self.is_animating == True:
+            self.current_sprite += 0.2
+
+            if self.current_sprite >= len(self.sprites):
+                self.current_sprite = 0
+                self.is_animating = False
+
+            self.image = self.sprites[int(self.current_sprite)]
+
 
 
 pygame.init()
@@ -37,17 +82,29 @@ pygame.display.set_caption("anime")
 
 
 moving_sprites = pygame.sprite.Group()
-player = Player(100, 100)
-moving_sprites.add(player)
+scuda = Scuda(100, 100)
+moving_sprites.add(scuda)
+
+#moving_sprites2 = pygame.sprite.Group()
+boat = Boat(200, 100)
+#moving_sprites2.add(boat)
+moving_sprites.add(boat)
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
+        PRESSED_KEYS = pygame.key.get_pressed()
+
+        if event.type == pygame.KEYDOWN:
+            scuda.animate()
+            boat.animate()
 
     # drawing
     screen.fill((0,0,0))
+
     moving_sprites.draw(screen)
+    moving_sprites.update()
     pygame.display.flip()
     clock.tick(60)
