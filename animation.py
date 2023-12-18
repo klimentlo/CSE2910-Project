eab247ff5e5bc42c3099a5d0e20ba6e11d0a4bcd
s.tran18 <s.tran18>
2023-12-18 11:20:15 -0700

    oct

--- animation.py
+++ animation.py
@@ -1,26 +1,16 @@
 import pygame
 
-class Scuda(pygame.sprite.Sprite):
+class Fish1(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
         super().__init__()
         self.sprites = []
         self.is_animating = False
-        self.sprites.append(pygame.image.load('media/00-removebg-preview.png'))
-        self.sprites.append(pygame.image.load('media/01-removebg-preview.png'))
-        self.sprites.append(pygame.image.load('media/02-removebg-preview.png'))
-        self.sprites.append(pygame.image.load('media/03-removebg-preview.png'))
-        self.sprites.append(pygame.image.load('media/10-removebg-preview.png'))
-        self.sprites.append(pygame.image.load('media/11-removebg-preview.png'))
-        self.sprites.append(pygame.image.load('media/12-removebg-preview.png'))
-        self.sprites.append(pygame.image.load('media/13-removebg-preview.png'))
-        self.sprites.append(pygame.image.load('media/20-removebg-preview.png'))
-        self.sprites.append(pygame.image.load('media/21-removebg-preview.png'))
-        self.sprites.append(pygame.image.load('media/22-removebg-preview.png'))
-        self.sprites.append(pygame.image.load('media/23-removebg-preview.png'))
-        self.sprites.append(pygame.image.load('media/30-removebg-preview.png'))
-        self.sprites.append(pygame.image.load('media/31-removebg-preview.png'))
-        self.sprites.append(pygame.image.load('media/32-removebg-preview.png'))
-        self.sprites.append(pygame.image.load('media/33-removebg-preview.png'))
+        self.sprites.append(pygame.image.load('media/00.png'))
+        self.sprites.append(pygame.image.load('media/10.png'))
+        self.sprites.append(pygame.image.load('media/20.png'))
+        self.sprites.append(pygame.image.load('media/30.png'))
+        self.sprites.append(pygame.image.load('media/40.png'))
+        self.sprites.append(pygame.image.load('media/50.png'))
         self.current_sprite = 0
         self.image = self.sprites[self.current_sprite]
 
@@ -42,13 +32,110 @@ class Scuda(pygame.sprite.Sprite):
 
             self.image = self.sprites[int(self.current_sprite)]
 
-class Boat(pygame.sprite.Sprite):
+class Octattack(pygame.sprite.Sprite):
     def __init__(self, pos_x, pos_y):
         super().__init__()
         self.sprites = []
         self.is_animating = False
-        self.sprites.append(pygame.image.load('media/submarine2.png'))
-        self.sprites.append(pygame.image.load('media/submarine4.png'))
+        self.sprites.append(pygame.image.load('media/octopus/oct1attack.png'))
+        self.sprites.append(pygame.image.load('media/octopus/oct2attack.png'))
+        self.sprites.append(pygame.image.load('media/octopus/oct3attack.png'))
+        self.sprites.append(pygame.image.load('media/octopus/oct4attack.png'))
+        self.sprites.append(pygame.image.load('media/octopus/oct5attack.png'))
+        self.sprites.append(pygame.image.load('media/octopus/oct6attack.png'))
+        self.current_sprite = 0
+        self.image = self.sprites[self.current_sprite]
+
+        self.rect = self.image.get_rect()
+        self.rect.topleft = [pos_x, pos_y]
+
+
+    def animate(self):
+        self.is_animating = True
+
+
+    def update(self):
+        if self.is_animating == True:
+            self.current_sprite += 0.2
+
+            if self.current_sprite >= len(self.sprites):
+                self.current_sprite = 0
+                self.is_animating = False
+
+            self.image = self.sprites[int(self.current_sprite)]
+
+class Octmove(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__()
+        self.sprites = []
+        self.is_animating = False
+        self.sprites.append(pygame.image.load('media/octopus/oct1move.png'))
+        self.sprites.append(pygame.image.load('media/octopus/oct2move.png'))
+        self.sprites.append(pygame.image.load('media/octopus/oct3move.png'))
+        self.sprites.append(pygame.image.load('media/octopus/oct4move.png'))
+        self.sprites.append(pygame.image.load('media/octopus/oct5move.png'))
+        self.sprites.append(pygame.image.load('media/octopus/oct6move.png'))
+        self.current_sprite = 0
+        self.image = self.sprites[self.current_sprite]
+
+        self.rect = self.image.get_rect()
+        self.rect.topleft = [pos_x, pos_y]
+
+
+    def animate(self):
+        self.is_animating = True
+
+
+    def update(self):
+        if self.is_animating == True:
+            self.current_sprite += 0.2
+
+            if self.current_sprite >= len(self.sprites):
+                self.current_sprite = 0
+                self.is_animating = False
+
+            self.image = self.sprites[int(self.current_sprite)]
+
+
+class Octidle(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__()
+        self.sprites = []
+        self.is_animating = False
+        self.sprites.append(pygame.image.load('media/octopus/oct1idle.png'))
+        self.sprites.append(pygame.image.load('media/octopus/oct2idle.png'))
+        self.sprites.append(pygame.image.load('media/octopus/oct3idle.png'))
+        self.sprites.append(pygame.image.load('media/octopus/oct4idle.png'))
+        self.sprites.append(pygame.image.load('media/octopus/oct5idle.png'))
+        self.sprites.append(pygame.image.load('media/octopus/oct6idle.png'))
+        self.current_sprite = 0
+        self.image = self.sprites[self.current_sprite]
+
+        self.rect = self.image.get_rect()
+        self.rect.topleft = [pos_x, pos_y]
+
+
+    def animate(self):
+        self.is_animating = True
+
+
+    def update(self):
+        if self.is_animating == True:
+            self.current_sprite += 0.2
+
+            if self.current_sprite >= len(self.sprites):
+                self.current_sprite = 0
+                self.is_animating = False
+
+            self.image = self.sprites[int(self.current_sprite)]
+
+class Octdeath(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__()
+        self.sprites = []
+        self.is_animating = False
+        self.sprites.append(pygame.image.load('media/octopus/oct1death.png'))
+        self.sprites.append(pygame.image.load('media/octopus/oct2death.png'))
         self.current_sprite = 0
         self.image = self.sprites[self.current_sprite]
 
@@ -81,14 +168,30 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption("anime")
 
 
-moving_sprites = pygame.sprite.Group()
-scuda = Scuda(100, 100)
-moving_sprites.add(scuda)
+oct_moving_sprites = pygame.sprite.Group()
+#scuda = Scuda(100, 100)
+#moving_sprites.add(scuda)
 
 #moving_sprites2 = pygame.sprite.Group()
-boat = Boat(200, 100)
+#boat = Boat(200, 100)
 #moving_sprites2.add(boat)
-moving_sprites.add(boat)
+#moving_sprites.add(boat)
+
+#fish1 = Fish1(300,300)
+#moving_sprites.add(fish1)
+
+OCTATTACK = Octattack(100, 300)
+oct_moving_sprites.add(OCTATTACK)
+
+OCTMOVE = Octmove(100, 250)
+oct_moving_sprites.add(OCTMOVE)
+
+OCTIDLE = Octidle(100, 200)
+oct_moving_sprites.add(OCTIDLE)
+
+OCTDEATH = Octdeath(100, 150)
+oct_moving_sprites.add(OCTDEATH)
+
 
 while True:
     for event in pygame.event.get():
@@ -98,13 +201,16 @@ while True:
         PRESSED_KEYS = pygame.key.get_pressed()
 
         if event.type == pygame.KEYDOWN:
-            scuda.animate()
-            boat.animate()
+            #fish1.animate()
+            OCTATTACK.animate()
+            OCTMOVE.animate()
+            OCTIDLE.animate()
+            OCTDEATH.animate()
 
     # drawing
     screen.fill((0,0,0))
 
-    moving_sprites.draw(screen)
-    moving_sprites.update()
+    oct_moving_sprites.draw(screen)
+    oct_moving_sprites.update()
     pygame.display.flip()
     clock.tick(60)
