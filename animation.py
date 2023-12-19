67e8dec4eece0028c030b5483a1b412bcdf7c44c
s.tran18 <s.tran18>
2023-12-19 11:48:46 -0700

    added eels attack

--- animation.py
+++ animation.py
@@ -168,6 +168,40 @@ class Octdeath(pygame.sprite.Sprite):
             self.image = pygame.transform.scale(self.image, (100, 100))
 
 
+class Eelattack(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__()
+        self.sprites = []
+        self.is_animating = False
+        self.sprites.append(pygame.image.load('media/eel/eel1attack.png'))
+        self.sprites.append(pygame.image.load('media/eel/eel2attack.png'))
+        self.sprites.append(pygame.image.load('media/eel/eel3attack.png'))
+        self.sprites.append(pygame.image.load('media/eel/eel4attack.png'))
+        self.sprites.append(pygame.image.load('media/eel/eel5attack.png'))
+        self.sprites.append(pygame.image.load('media/eel/eel6attack.png'))
+        self.current_sprite = 0
+        self.image = self.sprites[self.current_sprite]
+        self.image = pygame.transform.scale(self.image, (100, 100))
+
+        self.rect = self.image.get_rect()
+        self.rect.topleft = [pos_x, pos_y]
+
+
+    def animate(self):
+        self.is_animating = True
+
+
+    def update(self):
+        if self.is_animating == True:
+            self.current_sprite += 0.2
+
+            if self.current_sprite >= len(self.sprites):
+                self.current_sprite = 0
+                self.is_animating = False
+
+            self.image = self.sprites[int(self.current_sprite)]
+            self.image = pygame.transform.scale(self.image, (100, 100))
+
 pygame.init()
 clock = pygame.time.Clock()
 
@@ -178,6 +212,7 @@ pygame.display.set_caption("anime")
 
 
 oct_moving_sprites = pygame.sprite.Group()
+eel_moving_sprites = pygame.sprite.Group()
 #scuda = Scuda(100, 100)
 #moving_sprites.add(scuda)
 
@@ -201,6 +236,8 @@ oct_moving_sprites.add(OCTIDLE)
 OCTDEATH = Octdeath(100, 150)
 oct_moving_sprites.add(OCTDEATH)
 
+EELATTACK = Eelattack(300, 300)
+eel_moving_sprites.add(EELATTACK)
 
 while True:
     for event in pygame.event.get():
@@ -215,9 +252,12 @@ while True:
             OCTMOVE.animate()
             OCTIDLE.animate()
             OCTDEATH.animate()
+            EELATTACK.animate()
 
     # drawing
     screen.fill((0,0,0))
+    eel_moving_sprites.draw(screen)
+    eel_moving_sprites.update()
 
     oct_moving_sprites.draw(screen)
     oct_moving_sprites.update()
