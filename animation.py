68c9d436a6c5143a4d31f82f5a349b44ba4fa884
s.tran18 <s.tran18>
2023-12-21 11:42:17 -0700

    added more features to the animations

--- animation.py
+++ animation.py
@@ -277,6 +277,224 @@ class Eelattack(pygame.sprite.Sprite):
     def getY(self):
         return self.__Y
 
+
+class Eelmove(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__()
+        self.sprites = []
+        self.is_animating = False
+        self.sprites.append(pygame.image.load('media/eel/eel1move.png'))
+        self.sprites.append(pygame.image.load('media/eel/eel2move.png'))
+        self.sprites.append(pygame.image.load('media/eel/eel3move.png'))
+        self.sprites.append(pygame.image.load('media/eel/eel4move.png'))
+        self.sprites.append(pygame.image.load('media/eel/eel5move.png'))
+        self.sprites.append(pygame.image.load('media/eel/eel6move.png'))
+
+        self.current_sprite = 0
+        self.image = self.sprites[self.current_sprite]
+        self.image = pygame.transform.scale(self.image, (100, 100))
+        self.image = pygame.transform.flip(self.image, True, False)
+
+        self.__X = pos_x
+        self.__Y = pos_y
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (pos_x, pos_y)
+
+
+    def animate(self):
+        self.is_animating = True
+
+
+    def update(self):
+        if self.is_animating == True:
+            self.current_sprite += 0.2
+
+            if self.current_sprite >= len(self.sprites):
+                self.current_sprite = 0
+                self.is_animating = False
+
+            self.image = self.sprites[int(self.current_sprite)]
+            self.image = pygame.transform.scale(self.image, (100, 100))
+            self.image = pygame.transform.flip(self.image, True, False)
+
+    def setX(self, X):
+        self.__X = X
+        self.rect.topleft = (self.__X, self.__Y)
+
+    def setY(self, Y):
+        self.__Y = Y
+        self.rect.topleft = (self.__X, self.__Y)
+
+    def getPOS(self):
+        return self.rect
+
+    def getX(self):
+        return self.__X
+
+    def getY(self):
+        return self.__Y
+
+
+class Eelidle(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__()
+        self.sprites = []
+        self.is_animating = False
+        self.sprites.append(pygame.image.load('media/eel/eel1idle.png'))
+        self.sprites.append(pygame.image.load('media/eel/eel2idle.png'))
+        self.sprites.append(pygame.image.load('media/eel/eel3idle.png'))
+        self.sprites.append(pygame.image.load('media/eel/eel4idle.png'))
+        self.current_sprite = 0
+        self.image = self.sprites[self.current_sprite]
+        self.image = pygame.transform.scale(self.image, (100, 100))
+        self.image = pygame.transform.flip(self.image, True, False)
+
+        self.__X = pos_x
+        self.__Y = pos_y
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (pos_x, pos_y)
+
+
+    def animate(self):
+        self.is_animating = True
+
+
+    def update(self):
+        if self.is_animating == True:
+            self.current_sprite += 0.2
+
+            if self.current_sprite >= len(self.sprites):
+                self.current_sprite = 0
+                self.is_animating = False
+
+            self.image = self.sprites[int(self.current_sprite)]
+            self.image = pygame.transform.scale(self.image, (100, 100))
+            self.image = pygame.transform.flip(self.image, True, False)
+
+    def setX(self, X):
+        self.__X = X
+        self.rect.topleft = (self.__X, self.__Y)
+
+    def setY(self, Y):
+        self.__Y = Y
+        self.rect.topleft = (self.__X, self.__Y)
+
+    def getPOS(self):
+        return self.rect
+
+    def getX(self):
+        return self.__X
+
+    def getY(self):
+        return self.__Y
+
+
+class Eeldeath(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__()
+        self.sprites = []
+        self.is_animating = False
+        self.sprites.append(pygame.image.load('media/eel/eel1death.png'))
+        self.sprites.append(pygame.image.load('media/eel/eel2death.png'))
+        self.current_sprite = 0
+        self.image = self.sprites[self.current_sprite]
+        self.image = pygame.transform.scale(self.image, (100, 100))
+        self.image = pygame.transform.flip(self.image, True, False)
+
+        self.__X = pos_x
+        self.__Y = pos_y
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (pos_x, pos_y)
+
+
+    def animate(self):
+        self.is_animating = True
+
+
+    def update(self):
+        if self.is_animating == True:
+            self.current_sprite += 0.2
+
+            if self.current_sprite >= len(self.sprites):
+                self.current_sprite = 0
+                self.is_animating = False
+
+            self.image = self.sprites[int(self.current_sprite)]
+            self.image = pygame.transform.scale(self.image, (100, 100))
+            self.image = pygame.transform.flip(self.image, True, False)
+
+    def setX(self, X):
+        self.__X = X
+        self.rect.topleft = (self.__X, self.__Y)
+
+    def setY(self, Y):
+        self.__Y = Y
+        self.rect.topleft = (self.__X, self.__Y)
+
+    def getPOS(self):
+        return self.rect
+
+    def getX(self):
+        return self.__X
+
+    def getY(self):
+        return self.__Y
+
+
+class Swordattack(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__()
+        self.sprites = []
+        self.is_animating = False
+        self.sprites.append(pygame.image.load('media/sword/sword1attack.png'))
+        self.sprites.append(pygame.image.load('media/sword/sword2attack.png'))
+        self.sprites.append(pygame.image.load('media/sword/sword3attack.png'))
+        self.sprites.append(pygame.image.load('media/sword/sword4attack.png'))
+        self.sprites.append(pygame.image.load('media/sword/sword5attack.png'))
+        self.sprites.append(pygame.image.load('media/sword/sword6attack.png'))
+        self.current_sprite = 0
+        self.image = self.sprites[self.current_sprite]
+        self.image = pygame.transform.scale(self.image, (100, 100))
+        self.image = pygame.transform.flip(self.image, False, False)
+
+        self.__X = pos_x
+        self.__Y = pos_y
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (pos_x, pos_y)
+
+
+    def animate(self):
+        self.is_animating = True
+
+
+    def update(self):
+        if self.is_animating == True:
+            self.current_sprite += 0.2
+
+            if self.current_sprite >= len(self.sprites):
+                self.current_sprite = 0
+                self.is_animating = False
+
+            self.image = self.sprites[int(self.current_sprite)]
+            self.image = pygame.transform.scale(self.image, (100, 100))
+            self.image = pygame.transform.flip(self.image, False, False)
+
+    def setX(self, X):
+        self.__X = X
+        self.rect.topleft = (self.__X, self.__Y)
+
+    def setY(self, Y):
+        self.__Y = Y
+        self.rect.topleft = (self.__X, self.__Y)
+
+    def getPOS(self):
+        return self.rect
+
+    def getX(self):
+        return self.__X
+
+    def getY(self):
+        return self.__Y
 pygame.init()
 clock = pygame.time.Clock()
 
@@ -288,6 +506,7 @@ pygame.display.set_caption("anime")
 
 oct_moving_sprites = pygame.sprite.Group()
 eel_moving_sprites = pygame.sprite.Group()
+human_sword_moving_sprites = pygame.sprite.Group()
 #scuda = Scuda(100, 100)
 #moving_sprites.add(scuda)
 
@@ -314,6 +533,18 @@ oct_moving_sprites.add(OCTDEATH)
 EELATTACK = Eelattack(300, 300)
 eel_moving_sprites.add(EELATTACK)
 
+EELMOVE = Eelmove(300, 100)
+eel_moving_sprites.add(EELMOVE)
+
+EELIDLE = Eelidle(300, 200)
+eel_moving_sprites.add(EELIDLE)
+
+EELDEATH = Eeldeath(300, 150)
+eel_moving_sprites.add(EELDEATH)
+
+SWORDATTACK = Swordattack(300, 150)
+human_sword_moving_sprites.add(SWORDATTACK)
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -328,6 +559,10 @@ while True:
             OCTIDLE.animate()
             OCTDEATH.animate()
             EELATTACK.animate()
+            EELMOVE.animate()
+            EELIDLE.animate()
+            EELDEATH.animate()
+            SWORDATTACK.animate()
 
     # drawing
     screen.fill((0,0,0))
@@ -336,5 +571,8 @@ while True:
 
     oct_moving_sprites.draw(screen)
     oct_moving_sprites.update()
+
+    human_sword_moving_sprites.draw(screen)
+    human_sword_moving_sprites.update()
     pygame.display.flip()
     clock.tick(60)
