865998da3c71ca06eb88fdf37a8e51bb99ca0a32
Samuel Tran <s.tran18@share.epsb.ca>
2024-01-04 22:42:18 -0700

    added sword fish

--- animation.py
+++ animation.py
@@ -1423,12 +1423,326 @@ class Lighting(pygame.sprite.Sprite):
         return self.height
 
 
+class Swordfishattack(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__()
+        self.sprites = []
+        self.is_animating = False
+        self.sprites.append(pygame.image.load('media/swordfish/sfattack1.png'))
+        self.sprites.append(pygame.image.load('media/swordfish/sfattack2.png'))
+        self.sprites.append(pygame.image.load('media/swordfish/sfattack3.png'))
+        self.sprites.append(pygame.image.load('media/swordfish/sfattack4.png'))
+        self.sprites.append(pygame.image.load('media/swordfish/sfattack5.png'))
+        self.sprites.append(pygame.image.load('media/swordfish/sfattack6.png'))
+
+        self.current_sprite = 0
+        self.image = self.sprites[self.current_sprite]
+        self.height = 144
+        self.width = 144
+        self.image = pygame.transform.scale(self.image, (self.width, self.height))
+        self.image = pygame.transform.flip(self.image, True, False)
+
+
+        self.__X = pos_x
+        self.__Y = pos_y
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (pos_x, pos_y)
+
+#
+    def animate(self):
+        self.is_animating = True
+
+
+    def update(self):
+        if self.is_animating == True:
+            self.current_sprite += 0.2
+
+            if self.current_sprite >= len(self.sprites):
+                self.current_sprite = 0
+                self.is_animating = False
+
+            self.image = self.sprites[int(self.current_sprite)]
+            self.image = pygame.transform.scale(self.image, (self.width, self.height))
+            self.image = pygame.transform.flip(self.image, True, False)
+
+    def setX(self, X):
+        self.__X = X
+        self.rect.topleft = (self.__X, self.__Y)
+
+    def setY(self, Y):
+        self.__Y = Y
+        self.rect.topleft = (self.__X, self.__Y)
+
+    def setScale(self, SCALE_X, SCALE_Y=0):
+        '''
+        changes the image, making it bigger or smaller
+        :param SCALE_X: float
+        :param SCALE_Y: float
+        :return: none
+        '''
+        if SCALE_Y == 0:
+            SCALE_Y = SCALE_X
+
+        self.width = self.getWidth() * SCALE_X
+        self.height = self.getHeight() * SCALE_Y
+        self.image = pygame.transform.scale(self.image, (self.getWidth() * SCALE_X, self.getHeight() * SCALE_Y))
+
+
+    def getPOS(self):
+        return self.rect
+
+    def getX(self):
+        return self.__X
+
+    def getY(self):
+        return self.__Y
+
+    def getWidth(self):
+        return self.width
+
+    def getHeight(self):
+        return self.height
+
+
+
+class Swordfishdeath(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__()
+        self.sprites = []
+        self.is_animating = False
+        self.sprites.append(pygame.image.load('media/swordfish/sfdeath1.png'))
+        self.sprites.append(pygame.image.load('media/swordfish/sfdeath2.png'))
+
+        self.current_sprite = 0
+        self.image = self.sprites[self.current_sprite]
+        self.height = 144
+        self.width = 144
+        self.image = pygame.transform.scale(self.image, (self.width, self.height))
+        self.image = pygame.transform.flip(self.image, True, False)
+
+
+        self.__X = pos_x
+        self.__Y = pos_y
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (pos_x, pos_y)
+
+#
+    def animate(self):
+        self.is_animating = True
+
+
+    def update(self):
+        if self.is_animating == True:
+            self.current_sprite += 0.2
+
+            if self.current_sprite >= len(self.sprites):
+                self.current_sprite = 0
+                self.is_animating = False
+
+            self.image = self.sprites[int(self.current_sprite)]
+            self.image = pygame.transform.scale(self.image, (self.width, self.height))
+            self.image = pygame.transform.flip(self.image, True, False)
+
+    def setX(self, X):
+        self.__X = X
+        self.rect.topleft = (self.__X, self.__Y)
+
+    def setY(self, Y):
+        self.__Y = Y
+        self.rect.topleft = (self.__X, self.__Y)
+
+    def setScale(self, SCALE_X, SCALE_Y=0):
+        '''
+        changes the image, making it bigger or smaller
+        :param SCALE_X: float
+        :param SCALE_Y: float
+        :return: none
+        '''
+        if SCALE_Y == 0:
+            SCALE_Y = SCALE_X
+
+        self.width = self.getWidth() * SCALE_X
+        self.height = self.getHeight() * SCALE_Y
+        self.image = pygame.transform.scale(self.image, (self.getWidth() * SCALE_X, self.getHeight() * SCALE_Y))
+
+
+    def getPOS(self):
+        return self.rect
+
+    def getX(self):
+        return self.__X
+
+    def getY(self):
+        return self.__Y
+
+    def getWidth(self):
+        return self.width
+
+    def getHeight(self):
+        return self.height
+
+
+class Swordfishidle(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__()
+        self.sprites = []
+        self.is_animating = False
+        self.sprites.append(pygame.image.load('media/swordfish/sfidle1.png'))
+        self.sprites.append(pygame.image.load('media/swordfish/sfidle2.png'))
+        self.sprites.append(pygame.image.load('media/swordfish/sfidle3.png'))
+        self.sprites.append(pygame.image.load('media/swordfish/sfidle4.png'))
+
+        self.current_sprite = 0
+        self.image = self.sprites[self.current_sprite]
+        self.height = 144
+        self.width = 144
+        self.image = pygame.transform.scale(self.image, (self.width, self.height))
+        self.image = pygame.transform.flip(self.image, True, False)
+
+
+        self.__X = pos_x
+        self.__Y = pos_y
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (pos_x, pos_y)
+
+#
+    def animate(self):
+        self.is_animating = True
+
+
+    def update(self):
+        if self.is_animating == True:
+            self.current_sprite += 0.2
+
+            if self.current_sprite >= len(self.sprites):
+                self.current_sprite = 0
+                self.is_animating = False
+
+            self.image = self.sprites[int(self.current_sprite)]
+            self.image = pygame.transform.scale(self.image, (self.width, self.height))
+            self.image = pygame.transform.flip(self.image, True, False)
+
+    def setX(self, X):
+        self.__X = X
+        self.rect.topleft = (self.__X, self.__Y)
+
+    def setY(self, Y):
+        self.__Y = Y
+        self.rect.topleft = (self.__X, self.__Y)
+
+    def setScale(self, SCALE_X, SCALE_Y=0):
+        '''
+        changes the image, making it bigger or smaller
+        :param SCALE_X: float
+        :param SCALE_Y: float
+        :return: none
+        '''
+        if SCALE_Y == 0:
+            SCALE_Y = SCALE_X
+
+        self.width = self.getWidth() * SCALE_X
+        self.height = self.getHeight() * SCALE_Y
+        self.image = pygame.transform.scale(self.image, (self.getWidth() * SCALE_X, self.getHeight() * SCALE_Y))
+
+
+    def getPOS(self):
+        return self.rect
+
+    def getX(self):
+        return self.__X
+
+    def getY(self):
+        return self.__Y
+
+    def getWidth(self):
+        return self.width
+
+    def getHeight(self):
+        return self.height
+
+class Swordfishmove(pygame.sprite.Sprite):
+    def __init__(self, pos_x, pos_y):
+        super().__init__()
+        self.sprites = []
+        self.is_animating = False
+        self.sprites.append(pygame.image.load('media/swordfish/sfmove1.png'))
+        self.sprites.append(pygame.image.load('media/swordfish/sfmove2.png'))
+        self.sprites.append(pygame.image.load('media/swordfish/sfmove3.png'))
+        self.sprites.append(pygame.image.load('media/swordfish/sfmove4.png'))
+
+        self.current_sprite = 0
+        self.image = self.sprites[self.current_sprite]
+        self.height = 144
+        self.width = 144
+        self.image = pygame.transform.scale(self.image, (self.width, self.height))
+        self.image = pygame.transform.flip(self.image, True, False)
+
+
+        self.__X = pos_x
+        self.__Y = pos_y
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (pos_x, pos_y)
+
+#
+    def animate(self):
+        self.is_animating = True
+
+
+    def update(self):
+        if self.is_animating == True:
+            self.current_sprite += 0.2
+
+            if self.current_sprite >= len(self.sprites):
+                self.current_sprite = 0
+                self.is_animating = False
+
+            self.image = self.sprites[int(self.current_sprite)]
+            self.image = pygame.transform.scale(self.image, (self.width, self.height))
+            self.image = pygame.transform.flip(self.image, True, False)
+
+    def setX(self, X):
+        self.__X = X
+        self.rect.topleft = (self.__X, self.__Y)
+
+    def setY(self, Y):
+        self.__Y = Y
+        self.rect.topleft = (self.__X, self.__Y)
+
+    def setScale(self, SCALE_X, SCALE_Y=0):
+        '''
+        changes the image, making it bigger or smaller
+        :param SCALE_X: float
+        :param SCALE_Y: float
+        :return: none
+        '''
+        if SCALE_Y == 0:
+            SCALE_Y = SCALE_X
+
+        self.width = self.getWidth() * SCALE_X
+        self.height = self.getHeight() * SCALE_Y
+        self.image = pygame.transform.scale(self.image, (self.getWidth() * SCALE_X, self.getHeight() * SCALE_Y))
+
+
+    def getPOS(self):
+        return self.rect
+
+    def getX(self):
+        return self.__X
+
+    def getY(self):
+        return self.__Y
+
+    def getWidth(self):
+        return self.width
+
+    def getHeight(self):
+        return self.height
 
 if __name__ == "__main__":
     pygame.init()
     clock = pygame.time.Clock()
 
-    screen_width = 800
+    screen_width = 900
     screen_height = 600
     screen = pygame.display.set_mode((screen_width, screen_height))
     pygame.display.set_caption("anime")
@@ -1438,6 +1752,7 @@ if __name__ == "__main__":
     eel_moving_sprites = pygame.sprite.Group()
     human_sword_moving_sprites = pygame.sprite.Group()
     human_wand_moving_sprites = pygame.sprite.Group()
+    sword_fish_moving_sprites = pygame.sprite.Group()
 
     OCTATTACK = Octattack(144, 300)
     oct_moving_sprites.add(OCTATTACK)
@@ -1493,6 +1808,18 @@ if __name__ == "__main__":
     LIGHTING = Lighting(600, 300)
     eel_moving_sprites.add(LIGHTING)
 
+    SWORDFISHATTACK = Swordfishattack(700, 20)
+    sword_fish_moving_sprites.add(SWORDFISHATTACK)
+
+    SWORDFISHDEATH = Swordfishdeath(700, 150)
+    sword_fish_moving_sprites.add(SWORDFISHDEATH)
+
+    SWORDFISHIDLE = Swordfishidle(700, 300)
+    sword_fish_moving_sprites.add(SWORDFISHIDLE)
+
+    SWORDFISHMOVE = Swordfishmove(700, 400)
+    sword_fish_moving_sprites.add(SWORDFISHMOVE)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -1520,6 +1847,10 @@ if __name__ == "__main__":
                 WANDIDLE.animate()
                 BOMB.animate()
                 LIGHTING.animate()
+                SWORDFISHATTACK.animate()
+                SWORDFISHDEATH.animate()
+                SWORDFISHIDLE.animate()
+                SWORDFISHMOVE.animate()
 #
         # drawing
         screen.fill((0,0,0))
@@ -1534,5 +1865,9 @@ if __name__ == "__main__":
 
         human_wand_moving_sprites.draw(screen)
         human_wand_moving_sprites.update()
+
+        sword_fish_moving_sprites.draw(screen)
+        sword_fish_moving_sprites.update()
+
         pygame.display.flip()
         clock.tick(60)
